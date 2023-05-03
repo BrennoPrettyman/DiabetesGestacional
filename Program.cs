468def4b99930d10fc085fb279c50c655c3749c8
brennoprettyman <brennoprettyman@gmail.com>
2023-05-02 21:38:03 -0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DiabetesGestacional;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double Glicemia;
+
+        Console.Clear();
+
+        Console.Write("Glicemia (em mg/dL)...: ");
+        Glicemia = Convert.ToDouble(Console.ReadLine());
+
+        if (Glicemia < 92)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose...");
+            Console.ResetColor();
+        }
+
+        else if (Glicemia > 92 && Glicemia < 126)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Compatível com Diabetes Gestacional");
+            Console.ResetColor();
+        }
+
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Diabetes Mellitus na Gravidez");
+            Console.ResetColor();
+        }
+    }
+}
